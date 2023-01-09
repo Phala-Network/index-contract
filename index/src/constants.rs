1cd14cd5a4e973dd566612bfd7cc11d19c50ee38
tenheadedlion <16246256+tenheadedlion@users.noreply.github.com>
2023-01-09 11:42:28 +0800

    Implement phala to acala bridge executor (#34)

--- index/src/constants.rs
+++ index/src/constants.rs
@@ -2,3 +2,6 @@
 // pub(crate) const CHAINBRIDGE_ID_ETHEREUM: u8 = 0;
 // pub(crate) const CHAINBRIDGE_ID_MOONRIVER: u8 = 2;
 pub(crate) const CHAINBRIDGE_ID_PHALA: u8 = 3;
+pub(crate) const ACALA_PARACHAIN_ID: u32 = 2000;
+#[allow(dead_code)]
+pub(crate) const PHALA_PARACHAIN_ID: u32 = 2035;
